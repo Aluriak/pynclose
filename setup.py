01ede8bc216c54a38ba5f3b5fe5a131b5b0f5cb1
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-11-20 18:21:59 +0100

    move setup in setup.cfg, except extensions parts

--- setup.py
+++ setup.py
@@ -5,18 +5,10 @@ from distutils.core import setup, Extension
 # from pynclose import gen_finclose, gen_inclose
 from inclose_package import gen_inclose
 
-# module_finclose = os.path.join('build', 'finclose-binding.c')
-# with open(module_finclose, 'wt') as file_:
-    # print('Generating file {}'.format(module_finclose))
-    # gen_finclose.generate(file_)
 module_inclose = 'build/inclose-binding.cpp'
 with open(module_inclose, 'w') as file_:
     print('Generating file {}'.format(module_inclose))
     gen_inclose.generate(file_)
-
-# finclose = Extension('finclose',
-                     # sources = [module_finclose, 'build/my-module.c'],
-                     # include_dirs=['pynclose/finclose', 'build'])
 
 inclose = Extension('inclose',
                     # sources=[module_inclose],
@@ -26,12 +18,4 @@ inclose = Extension('inclose',
                     extra_link_args=['-fopenmp', '-O2', '-Wl,-z,defs', '-flto'],
                     language='c++')
 
-setup(name='pynclose',
-      version='0.0.1',
-      description='',
-      author='lucas bourneuf',
-      author_email='lucas.bourneuf@laposte.net',
-      ext_modules=[inclose],
-      packages=['pynclose'],
-     )
-
+setup(ext_modules=[inclose], packages=['pynclose'])
