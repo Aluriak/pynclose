539543794ca93773911fe389ff9746885b389969
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2019-02-25 18:17:20 +0100

    fixup! prove that the module can't be used for more than 1 search

--- pynclose/__init__.py
+++ pynclose/__init__.py
@@ -5,7 +5,7 @@ from . import extract
 
 
 def concepts(infile:str, min_ext:int, min_int:int) -> [(set, set)]:
-    import importlib
+    import importlib  # TODO: find a way to fix the bug described in run_test_dir/test_reusability_of_search.py
     importlib.reload(inclose)
     csv_file = inclose.run_search(infile, min_ext, min_int)
     yield from extract.from_csv_file(csv_file)
